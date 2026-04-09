ab8e0b7b2ad31c9901c7c746eb04d200765f2355
fatfathao <nt_hhh_gsminecraft@163.com>
2026-04-09 12:49:02 +0800

    fix: 引入langchain

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
-from pandasai.llm import Ollama
+from langchain_community.llms import Ollama
 
 class Settings:
     PROJECT_NAME: str = "HRBUST AI Plugins Microservice"
     
-    # 全局大模型单例
     @property
     def local_llm(self):
+        # 通过LangChain桥接大模型
         return Ollama(
             model="qwen2.5:32b",
             base_url="http://172.17.0.1:11434"
